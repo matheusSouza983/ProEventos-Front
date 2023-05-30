21317d59b8e3bf2891376119cd607d6167de8946
matheus souza <matheusgpc6@gmail.com>
2023-05-30 16:07:35 -0300

    fim da primeira secao

--- source/src/app/app.module.ts
+++ source/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { HttpClientModule } from '@angular/common/http'
 
@@ -17,7 +18,8 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
